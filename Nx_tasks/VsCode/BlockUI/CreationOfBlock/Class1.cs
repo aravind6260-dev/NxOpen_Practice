fb8c8bf1585167d8fd84f9c192f10f4b7b8b3264
aravind6260-dev <aravind6260@gmail.com>
2026-02-02 16:04:13 +0530

    SecondaryCommit

---
+++ Nx_tasks/VsCode/BlockUI/CreationOfBlock/Class1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NXOpen.Features;
+using NXOpen;
+
+namespace CreationOfBlock
+{
+    public class Class1
+    {
+       
+        public static void CreateBlock(Point3d originPoint, string length, string width, string height)
+        {
+            
+            /*Session theSession = Session.GetSession();
+            Part workPart = theSession.Parts.Work;
+            Part displayPart = theSession.Parts.Display;
+                      
+            Feature nullNXOpen_Features_Feature = null;
+            BlockFeatureBuilder blockFeatureBuilder1;
+            blockFeatureBuilder1 = workPart.Features.CreateBlockFeatureBuilder(nullNXOpen_Features_Feature);
+                         
+            Point3d originPoint1 = new Point3d(0.0, 0.0, 0.0);
+            blockFeatureBuilder1.SetOriginAndLengths(originPoint1, "100", "100", "100");
+                       
+            Feature feature1;
+            feature1 = blockFeatureBuilder1.CommitFeature();
+                                  
+            blockFeatureBuilder1.Destroy();*/
+
+            /*Session theSession = Session.GetSession();
+            Part workPart = theSession.Parts.Work;
+            Part displayPart = theSession.Parts.Display;
+
+            Feature nullNXOpen_Features_Feature = null;
+            BlockFeatureBuilder blockFeatureBuilder1;
+            blockFeatureBuilder1 = workPart.Features.CreateBlockFeatureBuilder(nullNXOpen_Features_Feature);*/
+
+           /*oint3d point3D = BlockCreation_1.originPoint;
+            double length = BlockCreation_1.length;
+            double width = BlockCreation_1.width;
+            double height = BlockCreation_1.height;
+            blockFeatureBuilder1.SetOriginAndLengths(point3D, length.ToString(), width.ToString(), height.ToString());*/
+            ;
+            //Point3d originPoint = new Point3d(0.0, 0.0, 0.0);
+            /*blockFeatureBuilder1.SetOriginAndLengths(originPoint, length, width, height);
+
+            Feature feature1;
+            feature1 = blockFeatureBuilder1.CommitFeature();
+
+            blockFeatureBuilder1.Destroy();*/
+            
+            try
+            {
+            Session theSession = Session.GetSession();
+            Part workPart = theSession.Parts.Work;
+            Part displayPart = theSession.Parts.Display;
+
+            Feature nullNXOpen_Features_Feature = null;
+            BlockFeatureBuilder blockFeatureBuilder1;
+            blockFeatureBuilder1 = workPart.Features.CreateBlockFeatureBuilder(nullNXOpen_Features_Feature);
+
+          
+            blockFeatureBuilder1.SetOriginAndLengths(originPoint, length, width, height);
+
+            Feature feature1;
+            feature1 = blockFeatureBuilder1.CommitFeature();
+
+            blockFeatureBuilder1.Destroy();
+            }
+            catch(Exception ex) 
+            {
+            UI.GetUI().NXMessageBox.Show("Error in CreateBlock Method ",NXMessageBox.DialogType.Error,ex.Message);
+            }
+            
+
+        }
+    }
+}
---
+++ Nx_tasks/VsCode/CylinderUI/CreationOfCylinder_1/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NXOpen;
+
+namespace CreationOfCylinder_1
+{
+    public class Class1
+    {
+        public static void CreateCylinder(Vector3d directions, Point3d originPoint, string diameter, string height)
+        {
+            try
+            {
+                NXOpen.Session theSession = NXOpen.Session.GetSession();
+                NXOpen.Part workPart = theSession.Parts.Work;
+
+                NXOpen.Features.Feature nullNXOpen_Features_Feature = null;
+                NXOpen.Features.CylinderBuilder cylinderBuilder1;
+                cylinderBuilder1 = workPart.Features.CreateCylinderBuilder(nullNXOpen_Features_Feature);
+
+
+                cylinderBuilder1.Diameter.RightHandSide = diameter;
+
+                cylinderBuilder1.Height.RightHandSide = height;
+                Point startpoint = workPart.Points.CreatePoint(originPoint);
+                cylinderBuilder1.Axis.Point = startpoint;
+                startpoint.RemoveViewDependency();
+                Direction manualDirection = workPart.Directions.CreateDirection(originPoint, directions, SmartObject.UpdateOption.WithinModeling);
+                cylinderBuilder1.Axis.Direction = manualDirection;
+
+                NXOpen.NXObject nXObject1;
+                nXObject1 = cylinderBuilder1.Commit();
+
+                cylinderBuilder1.Destroy();
+            }
+            catch (Exception ex)
+            {
+                UI.GetUI().NXMessageBox.Show("Error in CreateCylinder Method ", NXMessageBox.DialogType.Error, ex.Message);
+            }
+
+        }
+    }
+}

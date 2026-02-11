f7d722723e5f4e9be8d2f64cc27b813caba37a2a
aravind6260-dev <aravind6260@gmail.com>
2026-02-11 16:27:31 +0530

    3D_Stylers

---
+++ All_3D_BlockStylers/3D_Blocks/3D_DialogBlocks/ConeUI.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NXOpen;
+
+namespace CreationOfCone_1
+{
+    public class ConeUI
+    {
+        public static void CreateCone(Vector3d directions, Point3d originPoint, string baseDiameter, string topDiameter, string height)
+        {
+            try
+            {
+                NXOpen.Session theSession = NXOpen.Session.GetSession();
+                NXOpen.Part workPart = theSession.Parts.Work;
+
+                NXOpen.Features.Cone nullNXOpen_Features_Cone = null;
+                NXOpen.Features.ConeBuilder coneBuilder1;
+                coneBuilder1 = workPart.Features.CreateConeBuilder(nullNXOpen_Features_Cone);
+
+                coneBuilder1.BaseDiameter.RightHandSide = baseDiameter;
+
+                coneBuilder1.TopDiameter.RightHandSide = topDiameter;
+
+                coneBuilder1.Height.RightHandSide = height;
+                Point startpoint = workPart.Points.CreatePoint(originPoint);
+                startpoint.RemoveViewDependency();
+                coneBuilder1.Axis.Point = startpoint;
+                Direction manualDirection = workPart.Directions.CreateDirection(originPoint, directions, SmartObject.UpdateOption.WithinModeling);
+                //coneBuilder1.AxisDirection.RightHandSide = manualDirection;
+                coneBuilder1.Axis.Direction = manualDirection;
+
+                //coneBuilder1.HalfAngle.RightHandSide = "45";
+
+                NXOpen.NXObject nXObject1;
+                nXObject1 = coneBuilder1.Commit();
+
+                coneBuilder1.Destroy();
+            }
+            catch (Exception ex)
+            {
+                UI.GetUI().NXMessageBox.Show("Error in CreateCone Method ", NXMessageBox.DialogType.Error, ex.Message);
+            }
+
+        }
+
+    }
+}
---
+++ All_3D_BlockStylers/3D_Blocks/3D_DialogBlocks/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("3D_DialogBlocks")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MercedesBenz AG")]
+[assembly: AssemblyProduct("3D_DialogBlocks")]
+[assembly: AssemblyCopyright("Copyright © MercedesBenz AG 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5a12b89e-41e6-460e-b0c6-94023453f87e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
